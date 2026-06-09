18eae6044f89d3d269ece6f992dafce952972d24
Armando Mariscal <armando.marisscal@gmail.com>
2026-06-08 23:36:42 -0600

    feat(api): add comment filtering endpoints using Active Record queries

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,30 @@
 class CommentsController < ApplicationController
+  def api_index
+    render json: Comment.select(:commenter, :body)
+  end
+
+  def api_index_not_empty
+    render json: Comment.where.not(commenter: ["", nil])
+                        .where.not(body: ["", nil])
+                        .select(:commenter, :body)
+  end
+  
+  def api_index_without_body
+    render json: Comment.where(body: ["", nil])
+                        .select(:commenter, :body)
+  end
+
+  def api_index_without_commenter
+    render json: Comment.where(commenter: ["", nil])
+                        .select(:commenter, :body)
+  end
+
+  def api_index_incomplete
+    render json: Comment.where(commenter: ["", nil])
+                        .or(Comment.where(body: ["", nil]))
+                        .select(:commenter, :body)
+  end
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
